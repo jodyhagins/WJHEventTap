64d17acb9669508ffbcc9121d67193afcea25fb6
Jody Hagins <jody.hagins@cplusplusonline.com>
2015-09-17 19:07:18 -0400

    Initial Commit

---
+++ WJHEventTap/WJHEventTap.h
@@ -0,0 +1,19 @@
+//
+//  WJHEventTap.h
+//  WJHEventTap
+//
+//  Created by Jody Hagins on 9/17/15.
+//  Copyright (c) 2015 Jody Hagins. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WJHEventTap.
+FOUNDATION_EXPORT double WJHEventTapVersionNumber;
+
+//! Project version string for WJHEventTap.
+FOUNDATION_EXPORT const unsigned char WJHEventTapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WJHEventTap/PublicHeader.h>
+
+
